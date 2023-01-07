fabe58d6222b5c8fa4ae92553e9246e7d81a75d2
Arjun Pant <arjuntpant@gmail.com>
2023-01-07 12:12:30 -0500

    Changes

---
+++ button.py
@@ -0,0 +1,4 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("1600x1200")
--- wordle.py
+++ wordle.py
@@ -1,63 +1,76 @@
 import random
-import tkinter as Tk
+from tkinter import *
+
+root = Tk()
+root.geometry("1600x1200")
 
 def clickButton():
-  clickButton = Label(Tk,text="Welcome to Wordle! Guess a word with 5 letters. Then it will tell you what the letters are. If the letter is Green, that means the word has that letter in the same position. If it is yellow, that means that letter is in the word, but in a different position. If it is grey, that means the letter is not in the word. If the word is not in the dictionary, then don't guess it. if you guess the word in 6 tries or less, you win. If you guess the word in more tries, you lose. Press this text to start the game.")
+  clickButton = Label(root, text="Welcome to Wordle! Guess a word with 5 letters. Then it will tell you what the letters are. If the letter is Green, that means the word has that letter in the same position. If it is yellow, that means that letter is in the word, but in a different position. If it is grey, that means the letter is not in the word. If the word is not in the dictionary, then don't guess it. if you guess the word in 6 tries or less, you win. If you guess the word in more tries, you lose. Press this text to start the game.")
   clickButton.pack()
-StartButton = Button(text="Start Game", padx = 200, pady = 40)
-StartButton.pack()
-("Welcome to Wordle! Guess a word with 5 letters. Then it will tell you what the letters are. If the letter is Green, that means the word has that letter in the same position. If it is yellow, that means that letter is in the word, but in a different position. If it is grey, that means the letter is not in the word. If the word is not in the dictionary, then don't guess it. if you guess the word in 6 tries or less, you win. If you guess the word in more tries, you lose. Press this text to start the game.").replace(" ","\n")
-wordbank = ["Thorn", "Sharp", "Tiger", "Slyly", "Gamer", "Bunny", "Happy" , "Pooly" , "Grunt", "Piggy"]
+
+
+wordbank = ["Bunny" , "Thorn" , "Share" , "Spare" , "Stare", "Stair", "Snair", "Scare" , "Stain", "Spank", "Shank", "Shard"]
+
 word = (random.choice (wordbank))
 
 
 def letter_checker(guessletter, position, word):
   if guessletter == word[position]:
-    print (guessletter, "= Green")
-    return
+    #Label(root, text=guessletter+" = Green")
+    return "Green"
   else:
     for i in range(0, 5):
       if i == position:
         break;
       if guessletter == word[i]:
-        print (guessletter, "= Yellow")
-        return
-  print (guessletter, "= Grey")
+        #Label(root, text=guessletter+ " = Yellow")
+        return "Yellow"
+  #Label(root, text=guessletter+ " = Grey")
+  return "Grey"
     
 
 
 def SC(Guess):
   for i in range(0, 5):
     letter_checker(Guess[i], i, word)
-  
 
-FirstWord = input("Enter the first word: ")
+def pri():
+  e = Entry(root)
+  e.pack()
+  user = e.get()
+  myButton = Button(root, user, text="Submit") 
+  myButton.pack()
+  myButton.focus()
+
+FirstWord = pri()
 if FirstWord == word:
-  print ("You chose the correct word! ")
+  Label(root, text="You chose the correct word! ")
 else:
-  print (SC(FirstWord))
-  SecondWord = input("Enter the second word: ")
+  Label(root, text=SC(FirstWord))
+  SecondWord = pri()
   if SecondWord == word:
-    print ("You chose the correct word! ")
+    Label(root, text="You chose the correct word! ")
   else:
-    print (SC(SecondWord))
-    ThirdWord = input("Enter the third word: ")
+    Label(root, text=SC(SecondWord))
+    ThirdWord = pri()
     if ThirdWord == word:
-      print ("You chose the correct word! ")
+      Label(root, text="You chose the correct word! ")
     else:
-      print (SC(ThirdWord))
-      FourthWord = input("Enter the fourth word: ")
+      (SC(ThirdWord))
+      FourthWord = pri()
       if FourthWord == word:
-        print ("You chose the correct word! ")
+        Label(root, text="You chose the correct word! ")
       else:
         print (SC(FourthWord))
-        FifthWord = input("Enter the fifth word: ")
+        FifthWord = pri()
         if FifthWord == word:
-          print ("You chose the correct word! ")
+          Label(root, text="You chose the correct word! ")
         else:
           print (SC(FifthWord))
-          SixthWord = input("Enter the sixth word: ")
+          SixthWord = pri()
           if SixthWord == word:
-            print ("You chose the correct word! ")
+            Label(root, text="You chose the correct word! ")
           else:
-            print ("The word was" + word)
+            Label(root, text="The word was" + word)
+
+root.mainloop()
